b408b7b6622a514c411335e278b9bb136b9eab88
Stefan Spittank <stefan.spittank@codecentric.de>
2024-08-22 08:44:46 +0200

    useActionState

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
 import './App.css'
 import {addSubscription} from "./backend.ts";
+import { useActionState } from "react";
+
+// function App() {
+//    const [email, setEmail] = useState("");
+//    const [error, setError] = useState(null);
+//    const [isPending, setIsPending] = useState(false);
+//
+//    const handleSubmit = async () => {
+//      setIsPending(true);
+//      const error = await addSubscription(email);
+//      setIsPending(false);
+//      if (error) {
+//        setError(error);
+//        return;
+//      }
+//    };
+//
+//    return (
+//      <div>
+//        <label htmlFor="name">Email:</label>
+//        <input name="email" type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+//        <button onClick={handleSubmit} disabled={isPending}>
+//          Newsletter abonnieren
+//        </button>
+//        {error && <p>{error}</p>}
+//      </div>
+//    );
+// }
 
 function App() {
-   const [email, setEmail] = useState("");
-   const [error, setError] = useState(null);
-   const [isPending, setIsPending] = useState(false);
+      const [error, submitAction, isPending] = useActionState(
+        async (previousState, formData) => {
+          const error = await addSubscription(formData.get("name"));
+          if (error) {
+            return error;
+          }
+          return null;
+        },
+        null,
+      );
 
-   const handleSubmit = async () => {
-     setIsPending(true);
-     const error = await addSubscription(email);
-     setIsPending(false);
-     if (error) {
-       setError(error);
-       return;
-     }
-   };
-
-   return (
-     <div>
-       <label htmlFor="name">Email:</label>
-       <input name="email" type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
-       <button onClick={handleSubmit} disabled={isPending}>
-         Newsletter abonnieren
-       </button>
-       {error && <p>{error}</p>}
-     </div>
-   );
+      return (
+        <form action={submitAction}>
+          <label htmlFor="name">Email:</label>
+             <input name="email" type="email" />
+             <button type="submit" disabled={isPending}>
+               Newsletter abonnieren
+             </button>
+             {error && <p>{error}</p>}
+        </form>
+      );
 }
-
-
 
 
 export default App
